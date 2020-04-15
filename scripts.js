365df6d3b4fea5e26620acfba7c6d62e8e34a71a
Marguerite Kenedy <reach.marguerite@gmail.com>
2020-04-15 15:38:25 -0700

    add initial JS logic

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let startBtn = document.getElementById("start-btn");
+let nextBtn = document.getElementById("next-btn");
+let quizContainer = document.getElementById("quiz-container");
+let questionBox = document.getElementById("questio");
+let answers = document.getElementById("answer-btns");
+
+let shuffleQuestions, questionIndex;
+
+startBtn.addEventListener('click', startQuiz);
+nextBtn.addEventListener('click', () => {
+    questionIndex++
+    nextQuestion()    
+})
+
+function startQuiz() {
+    startBtn.classList.add('hide')
+    shuffleQuestions = questions.sort(() => Math.random() - .5)
+    questionIndex = 0
+    quizContainer.classList.remove('hide')
+    nextQuestion()
+}
+
+function nextQuestion() {
+    resetState()
+    showQuestions(shuffledQuestions[currentQuestionIndex])
+}
